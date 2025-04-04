59658b5936ba4eb676c770da9ff018b9b3593735
termer <termer@termer.net>
2025-04-04 12:23:19 -0400

    Multithreaded HTTP/1.1 server with Monoio sustains an average of over 120k RPS on my Ryzen 5 3600. I'm satisfied with that performance, so we can move forward with it as a baseline.

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,163 @@
-use bytes::Bytes;
-use http::Request;
-use monoio::net::{TcpListener, TcpStream};
-use monoio_http::h2::{
-    server::{self, SendResponse},
-    RecvStream,
-};
-
 mod cachestate;
 mod hash;
 
-use std::error::Error;
+use std::io;
+use std::num::NonZeroUsize;
+use std::os::fd::AsRawFd;
+use std::thread::available_parallelism;
+use http::{response::Builder, HeaderMap, StatusCode};
+use monoio::{io::{
+    sink::{Sink, SinkExt},
+    stream::Stream,
+    Splitable,
+}, net::{TcpListener, TcpStream}, IoUringDriver};
+use monoio::utils::bind_to_cpu_set;
+use monoio_http::{
+    common::{error::HttpError, request::Request, response::Response},
+    h1::{
+        codec::{decoder::RequestDecoder, encoder::GenericEncoder},
+        payload::{FixedPayload, Payload},
+    },
+    util::spsc::{spsc_pair, SPSCReceiver},
+};
 
-#[monoio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7081").unwrap();
-    println!("listening on {:?}", listener.local_addr());
+async fn thread_main() -> Result<(), io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:50002").unwrap();
+
+    unsafe {
+        let optval: libc::c_int = 1;
+        let ret = libc::setsockopt(
+            listener.as_raw_fd(),
+            libc::SOL_SOCKET,
+            libc::SO_REUSEPORT,
+            &optval as *const _ as *const libc::c_void,
+            size_of_val(&optval) as libc::socklen_t,
+        );
+        if ret != 0 {
+            return Err(io::Error::last_os_error());
+        }
+    }
+
+    println!("Listening");
+    loop {
+        let incoming = listener.accept().await;
+        match incoming {
+            Ok((stream, addr)) => {
+                //println!("accepted a connection from {}", addr);
+                monoio::spawn(handle_connection(stream));
+            }
+            Err(e) => {
+                println!("accepted connection failed: {}", e);
+            }
+        }
+    };
+}
+
+fn thread_launcher(core: usize) -> std::thread::JoinHandle<()> {
+    std::thread::spawn(move || {
+        bind_to_cpu_set([core]).expect("failed to set thread CPU bind");
+
+        monoio::RuntimeBuilder::<IoUringDriver>::new()
+            .enable_timer()
+            .build()
+            .expect("failed to build runtime")
+            .block_on(thread_main()).expect("failed to execute runtime function");
+    })
+}
+
+fn main() -> Result<(), io::Error> {
+    let thread_count = available_parallelism().map_or(1, NonZeroUsize::get);
+    println!("Starting with {thread_count} threads (one per logical CPU)");
+
+    let threads: Vec<_> = (0..thread_count).map(thread_launcher).collect();
+
+    // Wait for all threads to complete
+    for thread in threads {
+        thread.join().expect("failed to execute thread");
+    }
+
+    println!("All threads completed");
+
+    Ok(())
+}
+
+async fn handle_connection(stream: TcpStream) {
+    let (r, w) = stream.into_split();
+    let sender = GenericEncoder::new(w);
+    let mut receiver = RequestDecoder::new(r);
+    let (mut tx, rx) = spsc_pair();
+    monoio::spawn(handle_task(rx, sender));
 
     loop {
-        if let Ok((socket, _peer_addr)) = listener.accept().await {
-            println!("TCP connection accept");
-            monoio::spawn(async move {
-                if let Err(e) = serve(socket).await {
-                    println!("  -> err={:?}", e);
+        match receiver.next().await {
+            None => {
+                //println!("connection closed, connection handler exit");
+                return;
+            }
+            Some(Err(_)) => {
+                println!("receive request failed, connection handler exit");
+                return;
+            }
+            Some(Ok(item)) => match tx.send(item).await {
+                Err(_) => {
+                    println!("request handler dropped, connection handler exit");
+                    return;
                 }
-            });
+                Ok(_) => {
+                    //println!("request handled success");
+                }
+            },
         }
     }
 }
 
-async fn serve(socket: TcpStream) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let mut connection = server::handshake(socket).await.expect("Handshake failed");
-
-    while let Some(result) = connection.accept().await {
-        let (request, respond) = result?;
-        monoio::spawn(async move {
-            if let Err(e) = handle_request(request, respond).await {
-                println!("error while handling request: {}", e);
+async fn handle_task(
+    mut receiver: SPSCReceiver<Request>,
+    mut sender: impl Sink<Response, Error = impl Into<HttpError>>,
+) -> Result<(), HttpError> {
+    loop {
+        let request = match receiver.recv().await {
+            Some(r) => r,
+            None => {
+                return Ok(());
             }
-        });
+        };
+        let resp = handle_request(request).await;
+        sender.send_and_flush(resp).await.map_err(Into::into)?;
     }
-
-    println!("~~~~~~~~~~~ H2 connection CLOSE !!!!!! ~~~~~~~~~~~");
-    Ok(())
 }
 
-async fn handle_request(
-    mut request: Request<RecvStream>,
-    mut respond: SendResponse<Bytes>,
-) -> Result<(), Box<dyn Error + Send + Sync>> {
-    println!("GOT request: {:?}", request);
+async fn handle_request(req: Request) -> Response {
+    let mut headers = HeaderMap::new();
+    headers.insert("Server", "monoio-http-demo".parse().unwrap());
+    let mut has_error = false;
+    let mut has_payload = false;
+    let payload = match req.into_body() {
+        Payload::None => Payload::None,
+        Payload::Fixed(mut p) => match p.next().await.unwrap() {
+            Ok(data) => {
+                has_payload = true;
+                Payload::Fixed(FixedPayload::new(data))
+            }
+            Err(_) => {
+                has_error = true;
+                Payload::None
+            }
+        },
+        Payload::Stream(_) => unimplemented!(),
+    };
 
-    let body = request.body_mut();
-    while let Some(data) = body.data().await {
-        let data = data?;
-        println!("<<<< recv {:?}", data);
-        let _ = body.flow_control().release_capacity(data.len());
-    }
-
-    let response = http::Response::new(());
-    let mut send = respond.send_response(response, false)?;
-    println!(">>>> send");
-    send.send_data(Bytes::from_static(b"hello "), false)?;
-    send.send_data(Bytes::from_static(b"world\n"), true)?;
-
-    Ok(())
+    let status = if has_error {
+        StatusCode::INTERNAL_SERVER_ERROR
+    } else if has_payload {
+        StatusCode::OK
+    } else {
+        StatusCode::NO_CONTENT
+    };
+    Builder::new()
+        .status(status)
+        .header("Server", "monoio-http-demo")
+        .body(payload)
+        .unwrap()
 }
+
